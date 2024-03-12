7840cc6b82a4b164b75506de333f04d531c9ccd0
NazarBid <161229136+NazarBid@users.noreply.github.com>
2024-03-12 22:17:41 +0200

    Delete labs6 directory

--- labs6/labs6/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Agent,Property,Client,Request
-
-admin.site.register(Agent)
-admin.site.register(Property)
-admin.site.register(Client)
-admin.site.register(Request)
